3d26a3617da0d3524aef9beb4512f232de665240
bh7cw <bh7cw@gmail.com>
2019-11-11 17:31:00 -0500

    change mysql

--- MySQL/dc1.sql
+++ MySQL/dc1.sql
@@ -1,28 +1,28 @@
 -- create database
-CREATE DATABASE IF NOT EXISTS DayCare;
+CREATE DATABASE IF NOT EXISTS daycare;
 SHOW DATABASES;
 SHOW SCHEMAS;
-SHOW CREATE DATABASE DayCare;
-USE DayCare;
+SHOW CREATE DATABASE daycare;
+USE daycare;
 SELECT DATABASE();
 SELECT SCHEMA();
 
 -- create table student
 CREATE TABLE IF NOT EXISTS student(
-stuId INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-teacherId INT UNSIGNED DEFAULT '1',
-firstName VARCHAR(20) NOT NULL,
-lastName VARCHAR(20) NOT NULL,
+stu_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+teacher_id INT UNSIGNED DEFAULT '1',
+first_name VARCHAR(20) NOT NULL,
+last_name VARCHAR(20) NOT NULL,
 age TINYINT UNSIGNED NOT NULL,
 gender ENUM('M','F','S') NOT NULL,
 gpa DOUBLE(5,2),
-registerDate DATE NOT NULL
+register_date DATE NOT NULL
 )ENGINE=INNODB CHARSET=UTF8;
 DESC student;
 SHOW COLUMNS FROM student;
 
 -- insert data
-INSERT student(stuId, firstName, lastName, age, gender, registerDate) VALUES(1, 'Jimmy', 'Green', '12', '1', '2019-09-04'),
+INSERT student(stu_id, first_name, last_name, age, gender, register_date) VALUES(1, 'Jimmy', 'Green', '12', '1', '2019-09-04'),
 (2, 'Sally', 'Smith', '23', '2', '2018-09-04'),
 (3, 'Lizzy', 'Johnson', '62', '1', '2017-09-04'),
 (NULL, 'Markie', 'Williams', '32', '2', '2016-09-04'),
@@ -32,23 +32,23 @@ INSERT student(stuId, firstName, lastName, age, gender, registerDate) VALUES(1, 
 
 -- create table teacher
 CREATE TABLE IF NOT EXISTS teacher(
-teacherId INT UNSIGNED KEY AUTO_INCREMENT,
-firstName VARCHAR(20) NOT NULL,
-lastName VARCHAR(20) NOT NULL,
+teacher_id INT UNSIGNED KEY AUTO_INCREMENT,
+first_name VARCHAR(20) NOT NULL,
+last_name VARCHAR(20) NOT NULL,
 age TINYINT UNSIGNED NOT NULL,
 gender ENUM('M','F','S') NOT NULL,
 credits INT UNSIGNED,
 wage DOUBLE(8,2),
-registerDate DATE NOT NULL
+register_date DATE NOT NULL
 )ENGINE=INNODB CHARSET=UTF8;
 DESC teacher;
 SHOW COLUMNS FROM teacher;
 
 -- create table parent
 CREATE TABLE IF NOT EXISTS parent(
-stuId INT UNSIGNED KEY AUTO_INCREMENT,
-firstName VARCHAR(20) NOT NULL,
-lastName VARCHAR(20) NOT NULL,
+id INT UNSIGNED KEY AUTO_INCREMENT,
+first_name VARCHAR(20) NOT NULL,
+last_name VARCHAR(20) NOT NULL,
 age TINYINT UNSIGNED NOT NULL,
 gender ENUM('M','F','S') NOT NULL,
 phone VARCHAR(10) NOT NULL,
@@ -59,35 +59,35 @@ SHOW COLUMNS FROM parent;
 
 -- create table classroom
 CREATE TABLE IF NOT EXISTS classroom(
-classroomId INT UNSIGNED KEY AUTO_INCREMENT,
-classroomName VARCHAR(10)
+classroom_id INT UNSIGNED KEY AUTO_INCREMENT,
+classroom_name VARCHAR(10)
 )ENGINE=INNODB CHARSET=UTF8;
 DESC classroom;
 SHOW COLUMNS FROM classroom;
 
 -- create table classroom
 CREATE TABLE IF NOT EXISTS teacher_classroom(
-classroomId INT UNSIGNED KEY AUTO_INCREMENT,
-teacherId INT UNSIGNED
+classroom_id INT UNSIGNED KEY AUTO_INCREMENT,
+teacher_id INT UNSIGNED
 )ENGINE=INNODB CHARSET=UTF8;
 DESC teacher_classroom;
 SHOW COLUMNS FROM teacher_classroom;
 
 -- create table record
 CREATE TABLE IF NOT EXISTS record(
-stuId INT UNSIGNED,
-dose001 TINYINT UNSIGNED,
-dose002 TINYINT UNSIGNED,
-dose003 TINYINT UNSIGNED,
-dose004 TINYINT UNSIGNED,
-dose005 TINYINT UNSIGNED,
-dose006 TINYINT UNSIGNED,
-date001 DATE,
-date002 DATE,
-date003 DATE,
-date004 DATE,
-date005 DATE,
-date006 DATE
+stu_id INT UNSIGNED,
+dose_001 TINYINT UNSIGNED,
+date_001 DATE,
+dose_002 TINYINT UNSIGNED,
+date_002 DATE,
+dose_003 TINYINT UNSIGNED,
+date_003 DATE,
+dose_004 TINYINT UNSIGNED,
+date_004 DATE,
+dose_005 TINYINT UNSIGNED,
+date_005 DATE,
+dose_006 TINYINT UNSIGNED,
+date_006 DATE
 )ENGINE=INNODB CHARSET=UTF8;
 DESC record;
 SHOW COLUMNS FROM record;
@@ -95,8 +95,8 @@ SHOW COLUMNS FROM record;
 -- Hib, DTaP, Polio, HB, MMR, Varicella
 -- 1, 2, 3, 
 CREATE TABLE IF NOT EXISTS vaccine(
-vaccineId INT UNSIGNED,
-vaccineName CHAR(10)
+vaccine_id INT UNSIGNED,
+vaccine_name CHAR(10)
 )ENGINE=INNODB CHARSET=UTF8;
 DESC vaccine;
 SHOW COLUMNS FROM vaccine;
