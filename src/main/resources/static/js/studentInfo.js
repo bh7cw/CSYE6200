c31fc176b628d4556e65a4570599b8332aa45c88
wu.juan <jeanawu@163.com>
2019-11-15 01:35:11 -0500

    modify students register page

--- src/main/resources/static/js/studentInfo.js
+++ src/main/resources/static/js/studentInfo.js
@@ -4,7 +4,7 @@ var TableInit = function () {
     //初始化Table
     oTableInit.Init = function () {
         $('#stuInformation').bootstrapTable({
-            url: '',         //请求后台的URL（*）
+            url: 'https://www.fastmock.site/mock/da5a115445306fa7ff82be1eebbf20eb/integral/vaccine',         //请求后台的URL（*）
             method: 'get',                      //请求方式（*）
             toolbar: '#toolbar',                //工具按钮用哪个容器
             striped: true,                      //是否显示行间隔色
