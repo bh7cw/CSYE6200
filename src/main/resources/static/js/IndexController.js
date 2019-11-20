fa950eb78204a248840baa77f34cfbc2cf1a413c
kevin <mashirowhite333@gmail.com>
2019-11-19 22:41:28 -0500

    Update IndexController.js

--- src/main/resources/static/js/IndexController.js
+++ src/main/resources/static/js/IndexController.js
@@ -14,7 +14,7 @@ var TableInit = function () {
     //初始化Table
     oTableInit.Init = function () {
         $('#stuInformation').bootstrapTable({
-            url: '',         //moc url // （*）
+            url: 'http://localhost:9090/showHierarchy',         //moc url // （*）
             method: 'get',                      //请求方式（*）
             toolbar: '#toolbar',                //工具按钮用哪个容器
             striped: true,                      //是否显示行间隔色
